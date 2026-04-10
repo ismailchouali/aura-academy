016642acc0146bc28cd4602a9e28ed4e5a5c2b5c
choualiismail18 <choualiismail18@gmail.com>
2026-04-10 20:47:17 +0000

    81dc328a-b9da-411a-8a98-f7b2c186630c

--- src/components/views/schedule-view.tsx
+++ src/components/views/schedule-view.tsx
@@ -149,13 +149,13 @@ interface ConflictError {
 // Days array - populated inside component with translations
 function getDays(t: ReturnType<typeof useT>) {
   return [
-    { value: '1', label: t.days['1'], short: t.days.short['1'] },
-    { value: '2', label: t.days['2'], short: t.days.short['2'] },
     { value: '3', label: t.days['3'], short: t.days.short['3'] },
     { value: '4', label: t.days['4'], short: t.days.short['4'] },
     { value: '5', label: t.days['5'], short: t.days.short['5'] },
     { value: '6', label: t.days['6'], short: t.days.short['6'] },
     { value: '7', label: t.days['7'], short: t.days.short['7'] },
+    { value: '1', label: t.days['1'], short: t.days.short['1'] },
+    { value: '2', label: t.days['2'], short: t.days.short['2'] },
   ];
 }
 
