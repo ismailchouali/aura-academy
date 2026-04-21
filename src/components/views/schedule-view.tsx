7f45d84b04b45b696b033c4876cebf1f8a276331
choualiismail18 <choualiismail18@gmail.com>
2026-04-21 19:22:19 +0000

    fix: week starts on الإثنين

--- src/components/views/schedule-view.tsx
+++ src/components/views/schedule-view.tsx
@@ -144,16 +144,16 @@ interface ConflictError {
 
 // Days array - populated inside component with translations
 function getDays(t: ReturnType<typeof useT>) {
-  // In Morocco: Sunday is the first day of the week
-  // Right to left: الأحد, الإثنين, الثلاثاء, الأربعاء, الخميس, الجمعة, السبت
+  // Week starts on Monday
+  // Right to left: الإثنين, الثلاثاء, الأربعاء, الخميس, الجمعة, السبت, الأحد
   return [
-    { value: '1', label: t.days['1'], short: t.days.short['1'] },
     { value: '2', label: t.days['2'], short: t.days.short['2'] },
     { value: '3', label: t.days['3'], short: t.days.short['3'] },
     { value: '4', label: t.days['4'], short: t.days.short['4'] },
     { value: '5', label: t.days['5'], short: t.days.short['5'] },
     { value: '6', label: t.days['6'], short: t.days.short['6'] },
     { value: '7', label: t.days['7'], short: t.days.short['7'] },
+    { value: '1', label: t.days['1'], short: t.days.short['1'] },
   ];
 }
 
